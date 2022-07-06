4c24f978313572dca87d3491d147a01f8f8332cb
VuDej <drvu47@gmail.com>
2022-07-06 13:58:19 +0200

    implemented login user with specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
+    email { FFaker::Internet.email }
+    password { FFaker::Internet.password }
+    password_confirmation { password }
   end
 end
+
